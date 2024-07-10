5bbf3cda2a69c5b450541adda6b5df7272e83dea
billytran-tech <bautran.tech@gmail.com>
2024-07-10 22:34:23 +0200

    update unit test details

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,5 +7,6 @@ client = TestClient(app)
 
 def test_read_main():
     response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"msg": "Welcome to Suav Beauty"}
+    assert response.status_code == 200, f"Expected status code 200, but got {response.status_code}"
+    assert response.json() == {
+        "msg": "Welcome to Suav Beauty"}, f"Expected response body {{'msg': 'Welcome to Suav Beauty'}}, but got {response.json()}"
