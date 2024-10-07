b000f689c813ebc6cb74c640212ee34a9b6602f3
billytran-tech <bautran.tech@gmail.com>
2024-10-07 17:27:44 +0200

    add serviceDBObject model

--- app/schema/object_models/v0/service_model.py
+++ app/schema/object_models/v0/service_model.py
@@ -60,6 +60,21 @@ class ServiceOwner(BaseModel):
                               arbitrary_types_allowed=True)
 
 
+class ServiceDBObject(BaseModel):
+    id: str = Field(alias="_id")
+    service_name: str = Field(...)
+    duration_minutes: int = Field(...)
+    out_call: bool = Field(...)
+    description: str = Field(...)
+    images: Optional[List[str]] = None
+    price: Price = Field(...)
+    owner_id: str = Field(...)
+    # TODO[pydantic]: The following keys were removed: `json_encoders`.
+    # Check https://docs.pydantic.dev/dev-v2/migration/#changes-to-config for more information.
+    model_config = ConfigDict(
+        populate_by_name=True, arbitrary_types_allowed=True, json_encoders={ObjectId: str})
+
+
 class ServiceSnapshotResponse(BaseModel):
     id: str = Field(alias="_id")
     service_name: str = Field(...)
